34f0e30e458f3dda06475bf972a95ae4a7bcc2fd
sharanyaganguli <78684131+sharanyaganguli@users.noreply.github.com>
2021-06-12 16:27:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+var canvas= new fabric.canvas('myCanvas');
+
+block_height= "30";
+block_width="30";
+
+player_x="10";
+player_y="10";
+
+var player_object= "";
+
+function player_update();{
+    fabric.Image.fromUrl("player.png", function (Img){
+        player_object= Img;
+
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({
+
+            top: player_y,
+            left: player_x
+        });
+
+        canvas.add(player_object);
+    })
+}
+
+function new_image(get_image){
+    fabric.Image.fromUrl(get_image, function(Img){
+        block_object= Img;
+        block_object= scaleToWidth(block_width);
+        block_object= scaleToHeight(block_height);
+        block_object.set({
+            top: player_y, left:player_x
+        });
+        canvas.add(block_object);
+    });
+}
